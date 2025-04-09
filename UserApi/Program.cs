8b4ca620021e4ae013a2f28bb3c56f95c499cea1
Nikolas Kouis <kouis16cy@gmail.com>
2025-04-09 12:32:26 +0300

    Added event hub. Added test.html which waits for creation of user and projects info.

--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using UserApi.Data;
+using UserApi.Hubs;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,20 +9,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 // Register DbContext with PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//allow client, mostly for test.html to work
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClient", policy =>
+    {
+        policy
+            .WithOrigins("http://127.0.0.1:5500")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
 
+//app.UseHttpsRedirection();
+
+app.UseCors("AllowClient");
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<UserHub>("/hubs/user");
+
 
 app.Run();
